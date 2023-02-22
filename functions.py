e09b15bc17e5bc1c8b04da9e9336bd7ab84bbf16
walter <walter.lai001@umb.edu>
2023-02-22 13:41:39 -0500

    added interatomic distance calculator and the unit test

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
 import torch
 
 def interatomic_xyz_distances(coordinates):
-    return torch.tensor([])
+    return torch.unsqueeze(coordinates, dim=1) - coordinates  
--- tests.py
+++ tests.py
@@ -1,19 +1,30 @@
 import torch
 import functions
-
+#import pdb;pdb.set_trace()
 true_trajectory = torch.tensor([
- [0.539134,       0.110659,       -0.463559],
- [-0.518506,      0.485016,        0.053708],
- [0.079372,       -0.495675,       0.509850]
+
+ [[0, 0, 0],
+ [1.0576, -0.37436, -0.5173],
+ [0.459763,0.606335,-0.9734]],
+
+ [[-1.0576, 0.37436, 0.5173],
+  [0.00005,0,0],
+  [-0.59788,0.98069,-0.45614]],
+
+ [[-0.459763,-0.606335,0.9734],
+ [0.59788,-0.98069,0.45614],
+ [0,0,0]],
+
 ])
 
-actual_trajectory = functions.interatomic_xyz_distances(123)
 try:
-  torch.testing.assert_close(true_trajectory,actual_trajectory,msg='interatomic distances')
-except Exception as e:
-  print(e.args)
-
-try:
-  torch.testing.assert_close(true_trajectory,actual_trajectory)
+  coordinates = torch.tensor([
+  [0.5391356726,0.1106588251,-0.4635601962],
+  [-0.5185079933,0.4850176090,0.0537084789],
+  [0.0793723207,-0.4956764341,0.5098517173],
+  ])
+  actual_trajectory = functions.interatomic_xyz_distances(coordinates)
+  torch.testing.assert_close(true_trajectory,actual_trajectory,rtol=1e-04,atol=1e-04)
 except:
   print('interatomic distances function broke')
+  print(actual_trajectory)
