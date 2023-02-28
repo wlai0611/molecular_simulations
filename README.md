11e8c8b905e5304dcfdd045f5f165258bd7b8331
walter <walter.lai001@umb.edu>
2023-02-28 16:07:36 -0500

    corrected typo in Dr. Costa name

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-The script 000_simplest_md.c was written by Dr. Anthony Beardsworth Costa.  I used it as a base to add more functionality.
+The script 000_simplest_md.c was written by Dr. Anthony Costa.  I used it as a base to add more functionality.
--- simulation.py
+++ simulation.py
@@ -2,15 +2,15 @@ import torch
 import functions
 import cProfile
 
-coordinates = torch.rand(1000,3)*8
+coordinates = torch.rand(500,3)*20
 
-start = torch.cuda.Event(enable_timing=True)
-end   = torch.cuda.Event(enable_timing=True)
-start.record()
+#start = torch.cuda.Event(enable_timing=True)
+#end   = torch.cuda.Event(enable_timing=True)
+#start.record()
 
-#cProfile.run('functions.get_trajectories(coordinates)')
-print(functions.get_trajectories(coordinates))
-end.record()
-torch.cuda.synchronize()
-print(start.elapsed_time(end))
+cProfile.run('functions.get_trajectories(coordinates)')
+#print(functions.get_trajectories(coordinates))
+#end.record()
+#torch.cuda.synchronize()
+#print(start.elapsed_time(end))
 
